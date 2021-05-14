cf40eb3857539544ece4a4abf5eded1197baaf9d
cydonia0211 <cydonia0211@gmail.com>
2021-05-14 13:38:36 +0900

    1st update info-screen

--- info.php
+++ info.php
@@ -8,6 +8,7 @@
       rel="stylesheet"
       href="https://cdn.jsdelivr.net/gh/FortAwesome/Font-Awesome@5.14.0/css/all.min.css"
     />
+    <meta name="viewport" content="width=device-width, initial-sclae=1.0">
   </head>
   <body>
     <header></header>
@@ -21,6 +22,54 @@
     </a>
 
     <main class="info-screen area">
+      <div class="about-section">
+        <div class="inner-width">
+          <h1>About Us</h1>
+          <div class="border"></div>
+          <div class="about-section-row">
+            <div class="about-section-col">
+              <div class="about">
+                <p id="text-center">
+                  To meet Neighbor는 취업을 준비하는 학생들을 위한 커뮤니티 플랫폼입니다. 우리는 이 사이트를 통해 학생들에게 정보공유,
+                  스터디 구인, 소통의 장 마련 등 여러 기능을 제공하여 학생분들에게 조금이나마 도움이 되고자 합니다. 
+                </p>
+                <a class="more-btn" href="#">Read more</a>
+              </div>
+            </div>
+           <div class="about-section-col">
+              <div class="skills">
+                <p id="develop-lang">
+                  - 개발에 사용된 언어
+                </p>
+                <div class="skill">
+                  <div class="title">PHP</div>
+                  <div class="progress">
+                    <div class="progress-bar p1"><span>55.1%</span></div>
+                  </div>
+                </div>
+
+                <div class="skill">
+                  <div class="title">CSS</div>
+                  <div class="progress">
+                    <div class="progress-bar p2"><span>41.2%</span></div>
+                  </div>
+                </div>
+
+               <div class="skill">
+                  <div class="title">JavaScript</div>
+                  <div class="progress">
+                    <div class="progress-bar p3"><span>3.6%</span></div>
+                 </div>
+                </div>
+
+              </div>
+            </div>
+         </div>
+       </div>
+      </div>
+      
+
+      <!--
       <div class="info-title">
         <h1>About To meet Neighbor</h1>
       </div>
@@ -34,6 +83,8 @@
           </div>
         </div>
       </div>
+      !-->
+
     </main>
 
     <footer></footer>
