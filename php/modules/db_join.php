497810eb3353cd52417dfaa35157d0e5c16dc280
FIN443 <jjhun1228@gmail.com>
2021-05-16 15:54:58 +0900

    add check redundancy to join

--- php/modules/db_join.php
+++ php/modules/db_join.php
@@ -6,17 +6,18 @@
         $user_pass = $_POST["user_pass"];
         $user_name = $_POST["user_name"];
         $user_email = $_POST["user_email"];
-        if(empty($user_id) || empty($user_pass) || empty($user_name) || empty($user_email)) {
-            echo("
-            <script>
-                history.go(-1);
-            </script>
-            ");
-        } else {
+        $select_all = mysqli_query($connect, "SELECT * FROM users WHERE u_id = '$user_id'");
+        $row = mysqli_fetch_array($select_all);
+        if(!isset($row)) {
             $insert = mysqli_query($connect, "INSERT INTO `users` (`u_id`, `u_pass`, `u_nickname`, `u_email`, `u_role`) VALUES ('$user_id', '$user_pass', '$user_name', '$user_email', '0')");
             ?>
             <h1>가입되었습니다.</h1>
-            <a href="./../../index.php">돌아가기</a>
+            <a href="./../../index.php">메인으로 돌아가기</a>
+            <?php
+        } else {
+            ?>
+            <h1>이미 존재하는 아이디입니다.</h1>
+            <a href="./../../join.php">회원가입으로 돌아가기</a>
             <?php
         }
     }
